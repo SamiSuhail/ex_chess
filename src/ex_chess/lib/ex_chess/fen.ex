2298dd91887543f8aaea18984779b5d81dac066d
SamiSuhail <sami.suhail.dev@gmail.com>
2025-10-06 10:19:20 +0300

    ExChess.Fen

---
+++ src/ex_chess/lib/ex_chess/fen.ex
@@ -0,0 +1,98 @@
+defmodule ExChess.Fen do
+  alias ExChess.{Game, Board, Square, Piece}
+
+  @spec from_game(Game.t()) :: binary()
+  def from_game(%Game{
+        board: board,
+        active_color: active_color,
+        castling_rights: castling_rights,
+        en_passant_file: en_passant_file,
+        halfmove_clock: halfmove_clock,
+      }) do
+    "#{from_board(board)} #{from_active_color(active_color)} #{from_castling_rights(castling_rights)} #{from_en_passant_file(en_passant_file, active_color)} #{halfmove_clock} #{1}"
+  end
+
+  defp from_board(board) do
+    7..0//-1
+    |> Enum.map(&from_rank(&1, board))
+    |> Enum.join("/")
+  end
+
+  defp from_rank(rank, board) do
+    {empty_squares_clock, rank_fen_result} =
+      0..7
+      |> Enum.reduce({0, ""}, fn file, {empty_squares_clock, rank_fen_result} ->
+        piece = Board.get(board, Square.new(file, rank))
+
+        if is_nil(piece) do
+          {empty_squares_clock + 1, rank_fen_result}
+        else
+          updated_rank_fen_result =
+            rank_fen_result <> from_empty_squares(empty_squares_clock) <> from_piece(piece)
+
+          {0, updated_rank_fen_result}
+        end
+      end)
+
+    rank_fen_result <> from_empty_squares(empty_squares_clock)
+  end
+
+  defp from_empty_squares(0), do: ""
+  defp from_empty_squares(empty_squares_clock), do: "#{empty_squares_clock}"
+
+  defp from_piece(%Piece{type: :p, color: :white}), do: "P"
+  defp from_piece(%Piece{type: :p, color: :black}), do: "p"
+  defp from_piece(%Piece{type: :r, color: :white}), do: "R"
+  defp from_piece(%Piece{type: :r, color: :black}), do: "r"
+  defp from_piece(%Piece{type: :n, color: :white}), do: "N"
+  defp from_piece(%Piece{type: :n, color: :black}), do: "n"
+  defp from_piece(%Piece{type: :b, color: :white}), do: "B"
+  defp from_piece(%Piece{type: :b, color: :black}), do: "b"
+  defp from_piece(%Piece{type: :q, color: :white}), do: "Q"
+  defp from_piece(%Piece{type: :q, color: :black}), do: "q"
+  defp from_piece(%Piece{type: :k, color: :white}), do: "K"
+  defp from_piece(%Piece{type: :k, color: :black}), do: "k"
+
+  defp from_active_color(:white), do: "w"
+  defp from_active_color(:black), do: "b"
+
+  @ordered_castling_rights_mapping [
+    {:white_kingside?, "K"},
+    {:white_queenside?, "Q"},
+    {:black_kingside?, "k"},
+    {:black_queenside?, "q"},
+  ]
+  defp from_castling_rights(castling_rights) do
+    joined_castles =
+      @ordered_castling_rights_mapping
+      |> Enum.map(fn {key, symbol} -> if castling_rights[key], do: symbol, else: "" end)
+      |> Enum.join()
+
+    if joined_castles == "" do
+      "-"
+    else
+      joined_castles
+    end
+  end
+
+  defp from_en_passant_file(nil, _), do: "-"
+
+  defp from_en_passant_file(en_passant_file, active_color) do
+    rank =
+      case active_color do
+        :white -> "4"
+        :black -> "3"
+      end
+
+    "#{from_file(en_passant_file)}#{rank}"
+  end
+
+  defp from_file(0), do: "a"
+  defp from_file(1), do: "b"
+  defp from_file(2), do: "c"
+  defp from_file(3), do: "d"
+  defp from_file(4), do: "e"
+  defp from_file(5), do: "f"
+  defp from_file(6), do: "g"
+  defp from_file(7), do: "h"
+end
