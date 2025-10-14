4d771b3ad3fb480a718a3ddc7681d1d9c810119a
SamiSuhail <sami.suhail.dev@gmail.com>
2025-10-14 09:49:47 +0300

    revert bug

--- src/ex_chess/lib/ex_chess_core/piece_rules/linear_piece_rules.ex
+++ src/ex_chess/lib/ex_chess_core/piece_rules/linear_piece_rules.ex
@@ -1,5 +1,5 @@
 defmodule ExChessCore.PieceRules.LinearPieceRules do
-  alias ExChess.Square
+  alias ExChess.{Board, Square}
   alias ExChessCore.MoveContext
   import MoveContext
 
@@ -60,6 +60,9 @@ defmodule ExChessCore.PieceRules.LinearPieceRules do
 
       Square.same_location?(square, target_square) ->
         :ok
+
+      not Board.square_empty?(board, square) ->
+        {:error, :path_not_clear, %{collision_square: square}}
 
       true ->
         verify_path(
