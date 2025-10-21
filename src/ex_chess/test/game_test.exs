be45ba7a14dbdb01558f41799e6f44f2a8c13664
SamiSuhail <sami.suhail.dev@gmail.com>
2025-10-21 11:05:43 +0300

    add docs and doctests to `ExChess.Board`

--- src/ex_chess/test/game_test.exs
+++ src/ex_chess/test/game_test.exs
@@ -2,6 +2,7 @@ defmodule ExChessTest.GameTest do
   use ExUnit.Case
   alias ExChessTest.{Arrange, Assert}
   doctest ExChess
+  doctest ExChess.Board
   doctest ExChess.Move
   doctest ExChess.Square
   doctest ExChess.Piece
